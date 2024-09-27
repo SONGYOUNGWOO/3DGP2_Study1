265e8aa53340e1109a8a1bbb1556001eefe5122f
SONGYOUNGWOO <syw9846@gmail.com>
2024-09-27 12:57:47 +0900

    sky박스 마지막에 랜더링
    뎁스테스트를 true
    뎁스옵션을 lESS_EQUAL로 만듬 이미 있는 깊이 값보다 작을 때 통과
    스카이박스의
    output.position  을 xyww로 1로만듬

--- LabProject08-0-1/Shader.cpp
+++ LabProject08-0-1/Shader.cpp
@@ -260,9 +260,13 @@ D3D12_INPUT_LAYOUT_DESC CSkyBoxShader::CreateInputLayout()
 D3D12_DEPTH_STENCIL_DESC CSkyBoxShader::CreateDepthStencilState()
 {
 	D3D12_DEPTH_STENCIL_DESC d3dDepthStencilDesc;
-	d3dDepthStencilDesc.DepthEnable = FALSE;
+	//d3dDepthStencilDesc.DepthEnable = FALSE;
+	//d3dDepthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO;
+	//d3dDepthStencilDesc.DepthFunc = D3D12_COMPARISON_FUNC_NEVER;
+	d3dDepthStencilDesc.DepthEnable = TRUE;
 	d3dDepthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO;
-	d3dDepthStencilDesc.DepthFunc = D3D12_COMPARISON_FUNC_NEVER;
+	d3dDepthStencilDesc.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+
 	d3dDepthStencilDesc.StencilEnable = FALSE;
 	d3dDepthStencilDesc.StencilReadMask = 0xff;
 	d3dDepthStencilDesc.StencilWriteMask = 0xff;
